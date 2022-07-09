fa76253c8c371f5c28ea626e0e01b49f2c23bace
astianmuchui <sebastianmuchui79@gmail.com>
2022-07-09 15:30:51 +0300

    Initializing........

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+#include <DNSServer.h>
+#include <ESPAsyncWebServer.h>
+#ifdef ESP32
+#include <WiFi.h>
+
+#elif defined(ESP32)
+#include <ESP32WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
---
+++ src/main.ino
@@ -0,0 +1,3 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
